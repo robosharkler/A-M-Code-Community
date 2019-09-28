0ac5b5c73b20467cd3c292f0334bf50f6c778b98
shyuan7 <shyuan@tamu.edu>
2019-09-28 15:39:45 -0500

    publish.html

---
+++ src/main/java/com/codetogether/am/community/controller/PublishController.java
@@ -0,0 +1,12 @@
+package com.codetogether.am.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
