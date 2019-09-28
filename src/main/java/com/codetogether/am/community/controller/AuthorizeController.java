47f96a7936d9f42ff611ab108cbd81af54a00015
shyuan7 <shyuan@tamu.edu>
2019-09-28 11:08:23 -0500

    H2DB

--- src/main/java/com/codetogether/am/community/controller/AuthorizeController.java
+++ src/main/java/com/codetogether/am/community/controller/AuthorizeController.java
@@ -1,13 +1,18 @@
 package com.codetogether.am.community.controller;
 
+import com.codetogether.am.community.Model.User;
 import com.codetogether.am.community.dto.AccessTokenDTO;
 import com.codetogether.am.community.dto.GithubUser;
+import com.codetogether.am.community.mapper.UserMapper;
 import com.codetogether.am.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -21,9 +26,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
@@ -31,8 +40,21 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            //Success login
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //Fail login
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/codetogether/am/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.codetogether.am.community.mapper;
+
+import com.codetogether.am.community.Model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+}
